a9f6a241140d621642c04366de53deb9408ad60d
Luca Martin <luca.martin@tum.de>
2024-10-28 17:38:30 +0100

    Changed structure so it is proper header and cpp seperation

--- Chess/headers/Board.h
+++ Chess/headers/Board.h
@@ -8,67 +8,34 @@
 
 using BoardType = std::array<std::array<std::shared_ptr<Piece>, 8>, 8>;
 class Board {
- private:
-  BoardType board;
+    private:
+        BoardType board;
 
- public:
-  std::array<std::shared_ptr<Piece>, 8>& operator[](int row) {
-    return board[row];
-  }
+    public:
+         std::array<std::shared_ptr<Piece>, 8>& operator[](int row);
+         size_t size();
+      // void set(int row, int col, std::shared_ptr<Piece>& piece) {
+      //   board[row][col] = piece;
+      // }
 
-  // void set(int row, int col, std::shared_ptr<Piece>& piece) {
-  //   board[row][col] = piece;
-  // }
+      // std::shared_ptr<Piece>& get(int row, int col) { return board[row][col]; }
 
-  // std::shared_ptr<Piece>& get(int row, int col) { return board[row][col]; }
+         class Iterator {
+             /* Iterates through the pieces contained in the board */
+         public:
+             BoardType& board;
+             size_t i_row = 0;
+             size_t i_col = 0;
 
-  class Iterator
-      : std::iterator<std::input_iterator_tag, std::shared_ptr<Piece>, int,
-                      std::shared_ptr<Piece>, Piece> {
-    /* Iterates through the pieces contained in the board */
-   public:
-    BoardType& board;
-    size_t i_row = 0;
-    size_t i_col = 0;
+             Iterator(BoardType& board);
+             Iterator(BoardType& board, bool isEnd);
 
-    Iterator(BoardType& board) : board(board) {
-      if (operator*() == nullptr) {
-        ++(*this);
-      }
-    };
-    Iterator(BoardType& board, bool isEnd)
-        : board(board), i_row(board.size()){};
-
-    std::shared_ptr<Piece> operator*() {
-      if (i_row < board.size()) {
-        return board[i_row][i_col];
-      }
-      return nullptr;
-    }
-
-    Iterator& operator++() {
-      do {
-        if (i_col == board.size() - 1) {
-          ++i_row;
-          i_col = 0;
-        } else {
-          ++i_col;
-        }
-      } while (i_row < board.size() && board[i_row][i_col] == nullptr);
-      return *this;
-    }
-
-    bool operator==(Iterator& other) {
-      if (&board == &other.board) {
-        if (i_row == other.i_row && i_col == other.i_col) return true;
-      }
-      return false;
-    }
-
-    bool operator!=(Iterator& other) { return !operator==(other); }
-  };
-
+             Iterator& operator++();
+             bool operator==(const Iterator& other) const;
+             bool operator!=(const Iterator& other) const;
+             std::shared_ptr<Piece> operator*();
+         };
   Iterator begin();
   Iterator end();
-  size_t size() {return board.size();}
+
 };
--- Chess/src/Board.cpp
+++ Chess/src/Board.cpp
@@ -1,5 +1,49 @@
 #include "Board.h"
 
+std::array<std::shared_ptr<Piece>, 8>& Board::operator[](int row) {
+         return board[row];
+     }
+
+size_t Board::size() { return board.size();}
+
+Board::Iterator::Iterator(BoardType& board) : board(board) {
+    if (operator*() == nullptr) {
+        ++(*this);
+    }
+}
+
+Board::Iterator::Iterator(BoardType& board, bool isEnd)
+    : board(board), i_row(board.size()) {}
+
+std::shared_ptr<Piece> Board::Iterator::operator*() {
+    if (i_row < board.size()) {
+        return board[i_row][i_col];
+    }
+    return nullptr;
+}
+
+Board::Iterator& Board::Iterator::operator++() {
+    do {
+        if (i_col == board.size() - 1) {
+            ++i_row;
+            i_col = 0;
+        }
+        else {
+            ++i_col;
+        }
+    } while (i_row < board.size() && board[i_row][i_col] == nullptr);
+    return *this;
+}
+
+bool Board::Iterator::operator==(const Iterator& other) const {
+    return &board == &other.board && i_row == other.i_row && i_col == other.i_col;
+}
+
+bool Board::Iterator::operator!=(const Iterator& other) const {
+    return !(*this == other);
+}
+
+
 Board::Iterator Board::begin() { return Board::Iterator(board); }
 
 Board::Iterator Board::end() { return Board::Iterator(board, true); }

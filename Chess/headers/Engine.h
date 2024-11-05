8cd7b7832a1e7ee5f371568d56f0f7a095675319
Luca Martin <luca.martin@tum.de>
2024-11-05 12:08:45 +0100

    changed testing structure

--- Chess/headers/Engine.h
+++ Chess/headers/Engine.h
@@ -16,10 +16,12 @@ struct evalMove {
 class Engine {
 	
 	public:
+		Engine(bool testing_ = false) : testing(testing_) {};
 		static inline int checkctr = 0;
 		static inline int checkmatectr = 0;
 		static inline int capturectr = 0;
 		static inline int positionctr = 0;
+		const bool testing;
 		//Move findBestMove(Board &board, int depth);
 		int createTree(Board& board, int depth, sf::RenderWindow& renderwind,Move& bestMove);
 	    //int totalCalcDepth;
@@ -28,5 +30,6 @@ class Engine {
 		int evalPosition(Board& board);
 		bool updateCheckStatus(Board& board);
 		void countCaptures(Board& board,Coordinates move);
-		bool checkmateCounter(Board& board);
+		bool GameEndCheck(Board& board);
+		int giveNumPos(int depth);
 };
